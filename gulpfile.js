ab4fe93823980f569711e38896bdf67fcc2f3fb9
Hadi Yousufi <reach.hadiy@gmail.com>
2017-09-30 23:42:11 -0500

    adds gulp to project

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require("gulp"),
+    watch = require("gulp-watch"),
+    postcss = require("gulp-postcss"),
+    cssvars = require("postcss-simple-vars"),
+    autoprefixer = require("autoprefixer");
+
+gulp.task('default', function () {
+    console.log("Hooray Gulp works!");
+});
+
+gulp.task('html', function () {
+    console.log("Something useful done to your html here!");
+});
+
+gulp.task('styles', function () {
+    return gulp.src("./app/assets/styles/styles.css")
+        .pipe(postcss([cssvars, autoprefixer]))
+        .pipe(gulp.dest("./app/temp/styles"));
+});
+
+// WATCH
+gulp.task('watch', function () {
+    watch("./app/assets/styles/**/*.css", function () {
+        gulp.start("styles");
+    });
+});
