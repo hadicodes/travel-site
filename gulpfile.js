8b45d6f7b6866b7c719b8a2387a2c9f0db98a9ad
Hadi Yousufi <reach.hadiy@gmail.com>
2017-10-03 23:53:31 -0500

    adds modularized css files

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const gulp = require("gulp"),
     postcss = require("gulp-postcss"),
     cssvars = require("postcss-simple-vars"),
     nested = require("postcss-nested"),
+    cssImport = require("postcss-import");
     autoprefixer = require("autoprefixer");
 
 gulp.task('default', function () {
@@ -15,7 +16,7 @@ gulp.task('html', function () {
 
 gulp.task('styles', function () {
     return gulp.src("./app/assets/styles/styles.css")
-        .pipe(postcss([cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
         .pipe(gulp.dest("./app/temp/styles"));
 });
 
